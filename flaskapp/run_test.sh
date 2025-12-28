d695015fc9b3030db8e2e1e1791f39f2de211ac4
Platon-KH <vas92-92@mail.ru>
2025-12-28 15:21:32 +1000

    Update run_test.sh

--- flaskapp/run_test.sh
+++ flaskapp/run_test.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-# flaskapp/run_test.sh
 
 gunicorn --bind 127.0.0.1:5000 wsgi:app &
 APP_PID=$!
 
 sleep 5
 
-echo "Запуск клиентского теста..."
 python3 client.py
 APP_CODE=$?
 
 sleep 2
 
-echo "Остановка Gunicorn (PID: $APP_PID)..."
 kill -TERM $APP_PID
 
 exit $APP_CODE

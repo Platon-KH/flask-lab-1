18321f3a62e6c720f352ab3c4ea4cdb7b8845a38
Platon-KH <vas92-92@mail.ru>
2025-12-28 17:24:21 +1000

    Delete flaskapp directory

--- flaskapp/client.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-r1 = requests.get('http://127.0.0.1:5000/')
-assert r1.status_code == 200
-
-r2 = requests.get('http://127.0.0.1:5000/data_to')
-assert r2.status_code == 200
-
-print("OK")
